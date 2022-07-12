b50c238a818794b4d8728524c7f9f243c69df88a
Marenzers <marenzie@pm.me>
2022-07-12 11:32:46 -0500

    config.py - simplified fallback function, removed loggingDirectory and logFileName as config items. log.py - Removed loggingDirectory and logFileName as config items. Defined static replacements for these.

--- classes/config.py
+++ classes/config.py
@@ -7,6 +7,11 @@ __all__ = "Configuration",
 
 class Configuration:
     def __init__(self, filename: str):
+        """
+        Creates a configparser object from the filename provided. If no file actually exists by the name given,
+        one will be created
+        :param filename: The name of the file containing configuration data.
+        """
         self.filename = filename
         self.configuration = configparser.ConfigParser()
 
@@ -14,63 +19,55 @@ class Configuration:
             assert path.exists(f"{filename}")
         except AssertionError:
             warnings.warn(f"No {filename} file found. Generating a default one instead.", Warning)
-            self.fallback(everything=True)
+            self.fallback()
 
         try:
             self.configuration.read(filename)
         except MemoryError:
             raise MemoryError("Insufficient memory to import configuration file. The program cannot continue.")
 
-        try:
-            assert 'LOGGING' in self.configuration
-        except AssertionError:
-            warnings.warn(f"No LOGGING section in {filename} file. Generating a default one instead.",
-                          Warning)
-            self.fallback(category='LOGGING')
-
-        for key in ['logFileDirectory', 'logFileName', 'loggingLevel']:
-            try:
-                assert key in self.configuration['LOGGING']
-            except AssertionError:
-                warnings.warn(f"No {key} line found in {filename} file. Going with the default instead.",
-                              Warning)
-                self.fallback(category='LOGGING', item=key)
-
-        try:
-            assert self.configuration['LOGGING']['loggingLevel'] in ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
-        except AssertionError:
-            warnings.warn("Invalid logging level specified in config file. Reverting to WARNING instead.", Warning)
-            self.fallback(category='LOGGING', item='loggingLevel')
-
     def __getitem__(self, item):
+        """
+        Returns the imported or generated configuration settings when asked via classInstance['Category']['Item']
+        :param item: Automatically passed when you ask for items from this instance's dictionary.
+        :return: Configuration item requested
+        """
         return self.configuration[item]
 
-    def fallback(self, category: str = None, item: str = None, everything: bool = False):
+    def fallback(self, category: str = None, item: str = None):
+        """
+        Sets part or all of the configuration to default during runtime. Will create a default config file if both
+        keywords are None
+        :exception SyntaxError Occurs if you specify item but don't specify category
+        :keyword category Specify a string if you wish to set all values in the specified category to default
+        :keyword item Specify a string if you wish to set a specific item to default. Requires category to be string
+        :type category: str
+        :type item: str
+        """
         default_config = {
             "LOGGING": {
-                "logFileDirectory": "logs",
-                "logFileName": "cinnamonswirl.log",
                 "loggingLevel": "DEBUG"
             }
         }
-        # TODO: Can this be made a bit cleaner?
-        if everything:
-            for key in default_config.keys():
-                self.configuration[key] = {}
-                for subkey in default_config[key]:
-                    self.configuration[key][subkey] = default_config[key][subkey]
-            with open(file=self.filename, mode="w") as file:
-                self.configuration.write(file)
-            return
-        else:
-            assert category in default_config.keys()
-            if item is None:
-                self.configuration[category] = {}
-                for key in default_config[category].keys():
-                    self.configuration[category][key] = default_config[category][key]
-                with open(file=self.filename, mode="w") as file:
-                    self.configuration.write(file)
-                return
+        if category is None and item is not None:
+            raise SyntaxError("classes.config.Configuration.fallback must be either called with nothing, "
+                              "called with only a category or called with both a category and item.")
 
+        if category is None and item is None:
+            for key in default_config.keys():
+                for subkey in default_config[key].keys():
+                    self.configuration[key] = {}
+                    self.configuration[key][subkey] = default_config[key][subkey]
+                    with open(file=self.filename, mode="w") as file:
+                        self.configuration.write(file)
+
+        if category is not None and item is None:
+            assert category in default_config.keys()
+            self.configuration[category] = {}
+            for subkey in default_config[category].keys():
+                self.configuration[category][subkey] = default_config[category][subkey]
+
+        if category is not None and item is not None:
+            assert category in default_config.keys()
             assert item in default_config[category].keys()
             self.configuration[category][item] = default_config[category][item]
--- classes/log.py
+++ classes/log.py
@@ -1,6 +1,5 @@
-import logging
 import warnings
-from os import path, makedirs
+from os import path, makedirs, getcwd
 from classes import Configuration
 
 __all__ = "Log",
@@ -8,30 +7,49 @@ __all__ = "Log",
 
 class Log:
     def __init__(self, configuration_file: Configuration):
-        # In case the slashes here are confusing, it merges the two so we get path\to\directory\log.file
-        logging_file_path = f"{configuration_file['LOGGING']['logFileDirectory']}\\" \
-                              f"{configuration_file['LOGGING']['logFileName']}"
-        # TODO: There's a chance someone puts ..\..\path\to\somewhere\they\shouldn't\be
+        """
+        Validates configuration settings around logging, ensures the "logs" directory exists, creates it if it doesn't
+        :param configuration_file:
+        :return filepath for log file. Intended for use with Logging.basicConfig.
+        """
+        try:
+            assert 'LOGGING' in configuration_file.configuration
+        except AssertionError:
+            warnings.warn(f"No LOGGING section in {configuration_file.filename} file. "
+                          f"Generating a default one instead.",
+                          Warning)
+            configuration_file.fallback(category='LOGGING')
 
-        if not path.exists(logging_file_path):
+        for key in ['loggingLevel']:
             try:
-                directories_to_create = configuration_file['LOGGING']['logFileDirectory']
-                makedirs(f"{directories_to_create}")
-            except FileExistsError:
-                pass
-            except OSError:
-                warnings.warn("Invalid logging file path specified in config file. Reverting to default instead.",
+                assert key in configuration_file.configuration['LOGGING']
+            except AssertionError:
+                warnings.warn(f"No {key} line found in {configuration_file.filename} file. "
+                              f"Going with the default instead.",
                               Warning)
-                # TODO: Tell configuration_file to use default config
+                configuration_file.fallback(category='LOGGING', item=key)
 
-            try:
-                create_logging_file = open(f"{logging_file_path}", "x")
-            except OSError:
-                warnings.warn(f"The OS reports {logging_file_path} doesn't exist, but when we tried to create the file,"
-                              f" we were given an error. We will use a default config instead.")
-                # TODO: Tell configuration_file to use default config
+        try:
+            assert configuration_file.configuration['LOGGING']['loggingLevel'] in \
+                   ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+        except AssertionError:
+            warnings.warn("Invalid logging level specified in config file. Reverting to WARNING instead.", Warning)
+            configuration_file.fallback(category='LOGGING', item='loggingLevel')
 
+        current_path = getcwd()
+        logging_file_path = current_path + "\\logs\\cinnamonswirl.log"
+
+        try:
             assert path.exists(logging_file_path)
+        except AssertionError:
+            try:
+                directories_to_create, tail = logging_file_path.split("\\cinnamonswirl.log")
+                makedirs(f"{directories_to_create}")
+                open(f"{logging_file_path}", "x")
+            except OSError:
+                warnings.warn(f"Unable to create {logging_file_path}, verify you have permissions and/or disk space.",
+                              Warning)
+                raise
 
         self.path = logging_file_path
 
